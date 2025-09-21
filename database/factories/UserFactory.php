fee36fa4f2cef20333c830eac9e4b974c45eca83
Alan José <alanjsdelima@gmail.com>
2025-09-21 11:42:09 -0300

    Adding missing columns definition to UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,12 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'cpf' => fake()->unique()->numerify('###########'),
+            'profile_picture_path' => fake()->imageUrl(400, 400, 'people', true),
+            'phone_number' => fake()->numerify('55###########'),
+            'birth_date' => fake()->date(),
+            'is_active' => true,
+            // 'company_id',
         ];
     }
 
@@ -37,8 +43,15 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function inactive(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'is_active' => false,
         ]);
     }
 }
