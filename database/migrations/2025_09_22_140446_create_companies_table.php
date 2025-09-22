d06a0346a09d505297cc28b7d54fe096f849cf05
Alan José <alanjsdelima@gmail.com>
2025-09-22 12:36:21 -0300

    Adding create_companies_table migration

--- database/migrations/2025_09_22_140446_create_companies_table.php
+++ database/migrations/2025_09_22_140446_create_companies_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('fantasy_name');
+            $table->string('corporate_name');
+            $table->char('cnpj', 14)->unique();
+            $table->string('state_registration', 15)->nullable();
+            $table->string('logo_picture_path')->nullable();
+            $table->char('phone_number', 15)->nullable();
+            $table->string('landline_number', 15)->nullable();
+            $table->string('contact_email')->unique()->nullable();
+            $table->string('website_url')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
