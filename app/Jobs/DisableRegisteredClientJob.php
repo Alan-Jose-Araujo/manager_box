10a9ad0a8f2a269e2fe703af38476e19899b8f8a
Alan José <alanjsdelima@gmail.com>
2025-09-28 19:30:53 -0300

    Adding DisableRegisteredClientJob

---
+++ app/Jobs/DisableRegisteredClientJob.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Company;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class DisableRegisteredClientJob implements ShouldQueue
+{
+    use Queueable;
+
+    private Company $company;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(Company $company)
+    {
+        $this->company = $company;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        try {
+            $companyRelatedUsers = $this->company->users;
+            //TODO: Disable related items in stock.
+            foreach($companyRelatedUsers as $user) {
+                DB::table('sessions')->where('user_id', $user->id)->delete();
+                $user->update([
+                    'is_active' => false,
+                ]);
+            }
+            $this->company->update([
+                'is_active' => false,
+            ]);
+        } catch (\Exception $exception) {
+            Log::critical($exception);
+            return;
+        }
+    }
+}
