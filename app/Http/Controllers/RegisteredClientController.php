4b1b1811ff5b043d09b0fcfaf5196193ca4bc1c3
Alan José <alanjsdelima@gmail.com>
2025-09-28 12:52:58 -0300

    Adjusting RegisteredClientController

--- app/Http/Controllers/RegisteredClientController.php
+++ app/Http/Controllers/RegisteredClientController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Dtos\Composite\RegisteredClientCompositeDto;
 use App\Services\Composite\RegisteredClientService;
+use App\Traits\Traits\ExtractData;
 use Illuminate\Http\Request;
 use Log;
 
 class RegisteredClientController extends Controller
 {
+    use ExtractData;
+
     private RegisteredClientService $registeredClientService;
 
     public function __construct()
@@ -19,16 +22,37 @@ class RegisteredClientController extends Controller
     public function store(Request $request)
     {
         try {
-            $companyData = $request->input('company');
-            $userData = $request->input('user');
-            $userAddressData = $request->input('user_address');
-            $companyAddressData = $request->input('company_address', []);
+            $data = $request->all();
+
+            $companyData = array_filter(
+                $data,
+                fn($key) => str_starts_with($key, 'company_data_'),
+                ARRAY_FILTER_USE_KEY
+            );
+
+            $userData = array_filter(
+                $data,
+                fn($key) => str_starts_with($key, 'user_data_'),
+                ARRAY_FILTER_USE_KEY
+            );
+
+            $userAddressData = array_filter(
+                $data,
+                fn($key) => str_starts_with($key, 'user_address_data_'),
+                ARRAY_FILTER_USE_KEY
+            );
+
+            $companyAddressData = array_filter(
+                $data,
+                fn($key) => str_starts_with($key, 'company_address_data_'),
+                ARRAY_FILTER_USE_KEY
+            );
 
             $registeredClient = $this->registeredClientService->create([
-                'company' => $companyData,
-                'user' => $userData,
-                'user_address' => $userAddressData,
-                'company_address' => $companyAddressData
+                'company' => $this->replaceArrayKeysFragment($companyData, 'company_data_'),
+                'user' => $this->replaceArrayKeysFragment($userData, 'user_data_'),
+                'user_address' => $this->replaceArrayKeysFragment($userAddressData, 'user_address_data_'),
+                'company_address' => $this->replaceArrayKeysFragment($companyAddressData, 'company_address_data_'),
             ]);
 
             if ($registeredClient instanceof RegisteredClientCompositeDto) {
@@ -44,4 +68,5 @@ class RegisteredClientController extends Controller
             ], 500);
         }
     }
+    // ...existing code...
 }
