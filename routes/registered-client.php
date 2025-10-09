48b2bd0187b37b4557d88ce0deb74f21e5daccd6
Alan José <alanjsdelima@gmail.com>
2025-10-09 13:00:09 -0300

    Moving client routes to another file

---
+++ routes/registered-client.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Here are the routes related to the registered client:
+ * -- Company
+ * -- User
+ * -- Address
+ */
+
+use App\Http\Controllers\RegisteredClientController;
+use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Middleware\RoleMiddleware;
+
+Route::prefix('client')
+    ->group(function () {
+
+        /**
+         * Url: client/register
+         * HTTP Method: GET
+         * Controller: RegisteredClientController
+         * Controller Method: store
+         * Name: client.register
+         */
+        Route::get('register', [RegisteredClientController::class, 'store'])
+            ->middleware('guest')
+            ->name('client.register');
+
+        /**
+         * Url: client/update-company
+         * HTTP Method: PATCH
+         * Controller: RegisteredClientController
+         * Controller Method: updateCompany
+         * Name: client.update_company
+         */
+        Route::patch('/update-company', [RegisteredClientController::class, 'updateCompany'])
+            ->middleware(RoleMiddleware::using('company_admin'))
+            ->name('client.update_company');
+
+        /**
+         * Url: client/disable-account
+         * HTTP Method: PATCH
+         * Controller: RegisteredClientController
+         * Controller Method: disable
+         * Name: client.disable_account
+         */
+        Route::patch('/disable-account', [RegisteredClientController::class, 'disable'])
+        ->middleware(RoleMiddleware::using('company_admin'))
+        ->name('client.disable_account');
+
+    });
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\RegisteredClientController;
+/**
+ * Here are the routes related to application web context.
+ */
+
 use Illuminate\Support\Facades\Route;
-use Spatie\Permission\Middleware\RoleMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,16 +15,3 @@ Route::get('/csrf-token', function() {
         'csrf-token' => csrf_token(),
     ]];
 });
-
-// Client.
-
-Route::controller(RegisteredClientController::class)->group(function() {
-    Route::middleware('guest')->group(function() {
-        Route::post('/register', 'store')->name('client.register');
-    });
-
-    Route::middleware('auth')->group(function() {
-        Route::patch('/update-company', 'updateCompany')->middleware(RoleMiddleware::using('company_admin'));
-        Route::patch('/disable-account', 'disable')->middleware(RoleMiddleware::using('company_admin'));
-    });
-})->prefix('client');
