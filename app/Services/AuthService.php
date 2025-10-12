2851e638c52e56ae4de6a7d16c30ccf9ba9f6d4e
Alan José <alanjsdelima@gmail.com>
2025-10-12 09:04:44 -0300

    Adding AuthService

---
+++ app/Services/AuthService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Support\Facades\Auth;
+
+class AuthService
+{
+    /**
+     * @param array $credentials
+     * @param bool $remember
+     * @return bool
+     */
+    public function attemptToLogin(array $credentials, bool $remember = false): bool
+    {
+        return Auth::attempt($credentials, $remember);
+    }
+
+    /**
+     * @param \Illuminate\Auth\Authenticatable $user
+     * @param bool $remember
+     * @return void
+     */
+    public function loginWithUser(Authenticatable $user, bool $remember = false): void
+    {
+        Auth::login($user, $remember);
+    }
+
+    /**
+     * @return void
+     */
+    public function logout(): void
+    {
+        Auth::logout();
+    }
+}
