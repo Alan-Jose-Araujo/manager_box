25e53ec38b2a9b00f5d9358cabe4ec78dc14aa2e
Alan José <alanjsdelima@gmail.com>
2025-10-14 16:06:30 -0300

    Add unity of measure enum

---
+++ app/Enums/UnityOfMeasure.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Enums;
+
+enum UnityOfMeasure: string
+{
+    case AMPOLA = 'ampola';
+    case BALDE = 'balde';
+    case BANDEJA = 'bandeja';
+    case BARRA = 'barra';
+    case BISNAGA = 'bisnaga';
+    case BLOCO = 'bloco';
+    case BOBINA = 'bobina';
+    case BOMBONA = 'bombona';
+    case CAPSULA = 'capsula';
+    case CARTELA = 'cartela';
+    case CENTO = 'cento';
+    case CONJUNTO = 'conjunto';
+    case CM = 'cm';
+    case CM2 = 'cm2';
+    case CAIXA = 'caixa';
+    case CAIXA2 = 'caixa2';
+    case CAIXA3 = 'caixa3';
+    case CAIXA5 = 'caixa5';
+    case CAIXA10 = 'caixa10';
+    case CAIXA15 = 'caixa15';
+    case CAIXA20 = 'caixa20';
+    case CAIXA25 = 'caixa25';
+    case CAIXA50 = 'caixa50';
+    case CAIXA100 = 'caixa100';
+    case DISPLAY = 'display';
+    case DUZIA = 'duzia';
+    case EMBALAGEM = 'embalagem';
+    case FARDO = 'fardo';
+    case FOLHA = 'folha';
+    case FRASCO = 'frasco';
+    case GALAO = 'galao';
+    case GARRAFA = 'garrafa';
+    case GRAMAS = 'gramas';
+    case JOGO = 'jogo';
+    case KG = 'kg';
+    case KIT = 'kit';
+    case LATA = 'lata';
+    case LITRO = 'litro';
+    case M = 'm';
+    case M2 = 'm2';
+    case M3 = 'm3';
+    case MILHEIRO = 'milheiro';
+    case ML = 'ml';
+    case MWH = 'mwh';
+    case PACOTE = 'pacote';
+    case PALETE = 'palete';
+    case PARES = 'pares';
+    case PECA = 'peca'; // PEÇA
+    case POTE = 'pote';
+    case QUILATE = 'quilate';
+    case RESMA = 'resma';
+    case ROLO = 'rolo';
+    case SACO = 'saco';
+    case SACOLA = 'sacola';
+    case TAMBOR = 'tambor';
+    case TANQUE = 'tanque';
+    case TONELADA = 'tonelada';
+    case TUBO = 'tubo';
+    case UNIDADE = 'unidade';
+    case VASILHAME = 'vasilhame';
+    case VIDRO = 'vidro';
+}
