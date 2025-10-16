539c99e666e824e5394cd2faa61504c088226028
Alan José <alanjsdelima@gmail.com>
2025-10-16 10:36:14 -0300

    Adding itemsInStock relationship

--- app/Models/ItemInStockCategory.php
+++ app/Models/ItemInStockCategory.php
@@ -6,6 +6,7 @@ use App\Enums\ItemInStockCategoryScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ItemInStockCategory extends Model
@@ -42,5 +43,15 @@ class ItemInStockCategory extends Model
     public function warehouse(): BelongsTo
     {
         return $this->belongsTo(Warehouse::class, 'warehouse_id');
+    }
+
+    public function itemsInStock(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            ItemInStock::class,
+            'item_in_stock_has_category',
+            'item_in_stock_category_id',
+            'item_in_stock_id'
+        );
     }
 }
