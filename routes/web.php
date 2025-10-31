9cc0d7b1d71dd3260b73c391ace2d504a9be29ff
Brenno Pereira <brennovictor22@gmail.com>
2025-10-30 22:18:53 -0300

    feat: add custom 404 page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Livewire\LoginPage;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Register;
+use App\Livewire\NotFound;
 
 Route::get('/register', Register::class)->name('register');
 Route::get('/login',LoginPage::class)->name('login');
+Route::fallback(NotFound::class);
