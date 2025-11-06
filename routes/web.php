35bab101527fa99c9d84e2de63fb7478fbb76194
Alan José <alanjsdelima@gmail.com>
2025-11-06 09:26:02 -0300

    Adding provisory dashboard route

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect()->route('client.show_register_form');
 });
+
+
+// Provisory dashboard route.
+Route::get('/dashboard', function() {
+    return 'Olá';
+})->middleware(['custom.auth', 'verified'])->name('dashboard');
