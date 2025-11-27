f1b2b0bbe1c98857c82c6ca6040b908658f9e687
Alan José <alanjsdelima@gmail.com>
2025-11-27 15:35:28 -0300

    Adding favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/manager_box_icon.ico') }}">
     <title>{{ $title ?? 'Page Title' }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
