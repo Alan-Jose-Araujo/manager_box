170d8d28c5a073c499b9277ed711c33313c9794b
Alan José <alanjsdelima@gmail.com>
2025-12-04 12:43:59 -0300

    Fixing expired page error

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/x-icon" href="{{ asset('images/manager_box_icon.ico') }}">
     <title>{{ $title ?? 'Page Title' }}</title>
 
