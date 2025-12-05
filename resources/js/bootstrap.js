7a5a95aed445568340646c4873ff0a46abda474b
Alan José <alanjsdelima@gmail.com>
2025-12-05 15:06:25 -0300

    Registering chartjs no data plugin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,5 +5,22 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+const CHARTJS_NO_DATA_PLUGIN = {
+    id: 'noDataPlugin',
+    beforeDraw: (chart, args, options) => {
+        if (chart.data.datasets.every(ds => ds.data.length === 0)) {
+            const { ctx, chartArea: { top, bottom, left, right }, scales: { x, y } } = chart;
+            ctx.save();
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.font = '16px Arial';
+            ctx.fillStyle = 'grey';
+            ctx.fillText('Dados insuficientes para gerar relatórios.', (left + right) / 2, (top + bottom) / 2);
+            ctx.restore();
+        }
+    }
+};
+
 Chart.register(ChartDataLabels);
+Chart.register(CHARTJS_NO_DATA_PLUGIN);
 window.Chart = Chart;
