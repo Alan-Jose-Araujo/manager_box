77bb66f6ad240dabbd358fcc3eea18511b517088
Jobson Rafael <rafaeljobson06@gmail.com>
2025-12-11 00:06:41 -0300

    Implement AddNewStockForm - Tab1

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,10 @@ use App\Livewire\Welcome;
 use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', Welcome::class);
 
+Route::get('/dashboard', Dashboard::class)->name('dashboard');
 
-Route::get('/dashboard', App\Livewire\Dashboard::class);
+Route::get('/estoque/novo', function () {
+    return view('add-new-stock');
+})->name('stock.create');
