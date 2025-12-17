27b9bd32b267cfdea8e72b738ab3b4633a0b6444
Alan José <alanjsdelima@gmail.com>
2025-12-16 22:57:30 -0300

    Adding create item route

--- routes/stock.php
+++ routes/stock.php
@@ -4,6 +4,7 @@
  * Here are the routes related to the stock.
  */
 
+use App\Livewire\CreateStockItem;
 use App\Livewire\Stock\StockListing;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,15 @@ Route::prefix('stock')
          */
         Route::get('/list', StockListing::class)
             ->name('stock.list');
+
+        /**
+         * Url: stock/create-item
+         * HTTP Method: GET
+         * Middlewares: custom.auth, signed
+         * Component: CreateStockItem
+         * Name: stock.show_create_item_form
+         */
+        Route::get('/create-item', CreateStockItem::class)
+        ->name('stock.show_create_item_form');
 
     })->middleware(['custom.auth', 'signed']);
