c40baa29c4395978a7abc292501d02568d2878b2
Alan José <alanjsdelima@gmail.com>
2026-01-23 15:43:19 -0300

    Adding available options

--- app/Livewire/CreateStockItem.php
+++ app/Livewire/CreateStockItem.php
@@ -2,6 +2,10 @@
 
 namespace App\Livewire;
 
+use App\Enums\ItemInStockUnityOfMeasure;
+use App\Repositories\BrandRepository;
+use App\Repositories\WarehouseRepository;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -20,7 +24,7 @@ class CreateStockItem extends Component
     public $nome_comercial;
     public $sku;
     public $unidade_medida;
-    public $quantidade = 0;
+    public $quantidade = 1.00;
     public $quantidade_minima = 0;
     public $quantidade_maxima = 0;
     public $preco_custo;
@@ -32,6 +36,61 @@ class CreateStockItem extends Component
     public $marca;
     public $armazem;
     public $ativo = true; // toggle
+
+    public $unidades_medida_disponiveis = [];
+
+    public $marcas_disponiveis = [];
+
+    public $armazens_disponiveis = [];
+
+    public function mount()
+    {
+        $itemInStockUnitsOfMeasure = ItemInStockUnityOfMeasure::cases();
+        $brandsAvailable = (new BrandRepository())->paginate([
+            'company_id' => [
+                'operator' => '=',
+                'value' => Auth::user()->company_id,
+            ],
+            'is_active' => [
+                'logical' => 'and',
+                'operator' => '=',
+                'value' => true,
+            ],
+        ], 1000)->items();
+        $warehousesAvailable = (new WarehouseRepository())->paginate([
+            'company_id' => [
+                'operator' => '=',
+                'value' => Auth::user()->company_id,
+            ],
+            'is_active' => [
+                'logical' => 'and',
+                'operator' => '=',
+                'value' => true,
+            ]
+        ], 10000);
+
+        foreach($itemInStockUnitsOfMeasure as $unit) {
+            $this->unidades_medida_disponiveis[] = [
+                'name' => $unit->name,
+                'value' => $unit->value,
+            ];
+        }
+
+        foreach($brandsAvailable as $brand) {
+            $this->marcas_disponiveis[] = [
+                'name' => $brand->name,
+                'value' => $brand->id,
+            ];
+        }
+
+        foreach($warehousesAvailable as $warehouse) {
+            $this->armazens_disponiveis[] = [
+                'name' => $warehouse->name,
+                'value' => $warehouse->id,
+            ];
+        }
+
+    }
 
     /**
      * (wire:click).
